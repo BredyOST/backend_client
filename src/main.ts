8beb29eaa9a1e2a32e9465b0612e936b73ac7341
BredyOST <89292642644m@gmail.com>
2024-01-26 22:03:12 +0300

    Добавил новую функциональность

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,7 @@ import * as process from 'process';
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule, { cors: false });
   const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: [`${process.env['CLIENT_URL']}`],
-    credentials: true,
-  })
+  app.enableCors()
 
   // app.enableCors({ credentials: true, origin: true });
   // app.setGlobalPrefix('api');

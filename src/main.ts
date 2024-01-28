7514a1edd5c2dd16e46a28639a362d29b0312185
BredyOST <89292642644m@gmail.com>
2024-01-28 07:31:52 +0300

    в main сделал доп код для app.use

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as express from 'express'
 import { join } from 'path'
 import * as process from 'process'
-import session from 'express-session'
+import * as session from 'express-session';
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule, { cors: false });
   const app = await NestFactory.create(AppModule)
@@ -23,14 +23,6 @@ async function bootstrap() {
   )
 
   const config = new DocumentBuilder().setTitle('Клиент.ру').setVersion('1.0').addBearerAuth().build()
-
-  const document = SwaggerModule.createDocument(app, config)
-
-  // SwaggerModule.setup('swagger', app, document, {
-  //   swaggerOptions: {
-  //     persistAuthorization: true,
-  //   },
-  // })
 
   await app.listen(7777)
 }

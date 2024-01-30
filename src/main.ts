1bd3f5aeb62ded868faab704f8cd5e37188427dc
BredyOST <89292642644m@gmail.com>
2024-01-30 09:46:00 +0300

    доработка функционала

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
     // origin: ['https://xn--e1affem4a4d.com', `${process.env['API_IP']}`],
-      origin: `*`,
+    origin: `*`,
   })
 
   app.use(

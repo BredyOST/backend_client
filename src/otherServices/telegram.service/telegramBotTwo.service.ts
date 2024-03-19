ff6a3f7857445fc635a28bcc46861f91aab681cc
BredyOST <89292642644m@gmail.com>
2024-03-19 22:38:11 +0300

    доработка функционала

--- src/otherServices/telegram.service/telegramBotTwo.service.ts
+++ src/otherServices/telegram.service/telegramBotTwo.service.ts
@@ -203,7 +203,7 @@ export class TelegramTwoService implements OnApplicationShutdown {
       })
 
       // Ваша логика для отправки сообщения подтверждения пользователю
-      await this.bot.api.sendMessage( `${userId}`, `chatName ${link.invite_link}`);
+      await this.bot.api.sendMessage( `${userId}`, `${chatName} ${link.invite_link}`);
     } catch (error) {
       console.error("Error sending confirmation message:", error);
     }
